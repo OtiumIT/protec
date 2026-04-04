ebde5a7132016a1e292c96396965820936071206
tiago.correa <tisampaio.tsc@gmail.com>
2026-04-04 12:12:00 -0300

    fix: query JMESPath ECR URI no deploy workers

--- apps/api/src/version.generated.ts
+++ apps/api/src/version.generated.ts
@@ -2,4 +2,4 @@
  * Gerado em tempo de build por scripts/build-lambda.mjs
  */
 export const API_VERSION = '1.0.0';
-export const API_UPDATED_AT = '2026-04-04T14:59:44.553Z';
+export const API_UPDATED_AT = '2026-04-04T15:11:58.162Z';
